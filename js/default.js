9c8e761b583331d113f971878e5d7b585c53908f
Wallace Erick <wallace_erick@hotmail.com>
2013-08-22 22:26:01 -0300

    New content to test metatags

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@ $(function(){
         var hash = location.hash;
 
         //Change the Title
-        document.title = 'Hashchange | The current hash is ' + (hash.replace( /^#!/, '') || 'blank') + '.';
+        document.title = 'Hashchange Test | The current language is: ' + (hash.replace( /^#!/, '') || 'blank') + '.';
 
         //Bind Link's
         $('#nav li a').each(function(){
@@ -15,64 +15,48 @@ $(function(){
                 $('#nav li a[href="#!home"]').addClass('selected');
             }
             //Add selected class to Blog, when is a post!
-            if(hash == "#!blog/post-1" || hash == "#!blog/post-2"){
-                $('#nav li a[href="#!blog"]').addClass('selected');
+            if(hash == "#!other/jquery" || hash == "#!other/zepto"){
+                $('#nav li a[href="#!other"]').addClass('selected');
             }
         });
 
         function reset(){
             //All Pages
             $('.page').hide();
-            $('.page h2').animate({
-                opacity: 0, 
-                marginLeft: '0'
-            }, 500);
-            $('.page p').animate({
-                opacity: 0, 
-                marginLeft: '0'
-            }, 700);
         }
 
-        /* Home */
         if(hash == "#!home"){
             reset();
-            $('.home, .home h1').fadeIn(300);
-        } 
-        /* About */
-        else if(hash == "#!about"){
-            reset();
-            $('.about, .about h1').fadeIn(300);
+            $('.home').fadeIn(300);
         }
-        /* Portfolio */
-        else if(hash == "#!portfolio"){
+        else if(hash == "#!html"){
             reset();
-            $('.portfolio, .portfolio h1').fadeIn(300);
+            $('.html').fadeIn(300);
         }
-        /* Services */
-        else if(hash == "#!services"){
+        else if(hash == "#!css"){
             reset();
-            $('.services, .services h1').fadeIn(300);
+            $('.css').fadeIn(300);
         }
-        /* Blog */
-        else if(hash == "#!blog"){
+        else if(hash == "#!php"){
             reset();
-            $('.blog, .blog h1').fadeIn(300);
+            $('.php').fadeIn(300);
         }
-        /* Contact */
-        else if(hash == "#!contact"){
+        else if(hash == "#!javascript"){
             reset();
-            $('.contact, .contact h1').fadeIn(300);
+            $('.javascript').fadeIn(300);
+        }
+        else if(hash == "#!other"){
+            reset();
+            $('.other').fadeIn(300);
         }
 
-            /* Blog/Post 1 */
-            else if(hash == "#!blog/post-1"){
+            else if(hash == "#!other/jquery"){
                 reset();
-                $('.post-1, .post-1 h1').fadeIn(300);
-            } 
-            /* Blog/Post 2 */
-            else if(hash == "#!blog/post-2"){
+                $('.jquery').fadeIn(300);
+            }
+            else if(hash == "#!other/zepto"){
                 reset();
-                $('.post-2, .post-2 h1').fadeIn(300);
+                $('.zepto').fadeIn(300);
             }
 
     });
